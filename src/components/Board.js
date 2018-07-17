723521eed65057aa1300083883a96c2d15e77277
Jarosław Mejer <jaroslaw.mejer@gmail.com>
2018-07-17 12:55:10 +0200

    Add callback

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,10 +10,13 @@ class Board extends React.Component {
 
         };
     }
+    myCallback(dataFromTile){
+        this.value = dataFromTile
+    }
     render() {
         return (
             <div className={style.boardContainer}>
-                {this.props.boardState.split('').map(eachTile => <Tile value={eachTile} key={uuid.v4()} className={style.tile} />)}
+                {this.props.boardState.split('').map(eachTile => <Tile callbackFromParent={this.myCallback} value={eachTile} key={uuid.v4()} className={style.tile} />)}
             </div>
         )
     }
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -7,10 +7,10 @@ class Tile extends React.Component {
             value:''
         };
         this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
     }
     handleChange(event) {
-        this.setState({value: event.target.value})
+        this.props.callbackFromParent(event.target.value)
+        console.log(event.target.value + '' + this.props.value)
     }
     handleSubmit(event) {
         console.log('Number ' + this.state.value + ' was submitted')
